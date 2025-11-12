3dd910cbcafc22f893e25e371157c8c852225222
hoangdoan8222-hash <hoangdoan8222@gmail.com>
2025-11-12 23:27:28 +0700

    Add files via upload

---
+++ Doan Huy Hoang_session15_BTVN4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int tongChuSo(int n) {
+    int tong = 0;
+    if (n < 0) n = -n; 
+
+    while (n > 0) {
+        tong += n % 10; 
+        n /= 10;        
+    }
+    return tong;
+}
+
+int main() {
+    int n;
+    printf("Nhap mot so nguyen: ");
+    scanf("%d", &n);
+
+    printf("Tong cac chu so cua %d la: %d\n", n, tongChuSo(n));
+
+    return 0;
+}
+
